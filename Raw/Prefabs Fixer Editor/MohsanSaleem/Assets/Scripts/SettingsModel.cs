de58e41ad6af6dfb4edada99cbbcb410e5af492a
Mohsan Saleem <mohsansaleem@gmail.com>
2019-11-19 00:50:11 +0500

    Data

---
+++ Raw/Prefabs Fixer Editor/MohsanSaleem/Assets/Scripts/SettingsModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class SettingsModel
+{
+    public string description;
+    public string SpritePath;
+    public Vector4 Color;
+
+    [System.NonSerialized] public Sprite _sprite;
+    [System.NonSerialized] public Color _color;
+
+ //   [System.NonSerialized] public SettingsModel _history;
+ //   [System.NonSerialized] public bool _isPropertyApplied;
+
+    public void LoadAssets()
+    {
+        _color = Color;
+        _sprite = AssetDatabase.LoadAssetAtPath<Sprite>(SpritePath);
+    }
+
+    public void SaveAssetsData()
+    {
+        SpritePath = AssetDatabase.GetAssetPath(_sprite);
+        Color = _color;
+    }
+}
+
+[System.Serializable]
+public class Settings
+{
+    public List<SettingsModel> SettingsList;
+}
---
+++ Raw/Prefabs Fixer Editor/MohsanSaleem/Assets/serilizationTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class serilizationTest : MonoBehaviour
+{
+    string json;
+   [ContextMenu("serailze")]
+    void Start()
+    {
+        SettingsModel item = new SettingsModel() { description = "Potion of Mana" , SpritePath =  "/data/filename", Color = new Vector4(1,0,1,1) };
+
+        Settings setin = new Settings();
+        setin.SettingsList = new List<SettingsModel>();
+        setin.SettingsList.Add(item);
+        json = JsonUtility.ToJson(setin);
+
+        Debug.Log(json);
+        string path = "Assets/Prefab";
+        Debug.Log(path.Substring("Assets".Length));
+    }
+
+    [ContextMenu("deseralize")]
+    void Update()
+    {
+        var myObject = JsonUtility.FromJson<SettingsModel>(json);
+        Debug.Log(myObject.description);
+    }
+}
